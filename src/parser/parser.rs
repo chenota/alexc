b1bd94c70997a8aff3f8daec66b42add9f2ee49a
chenota <achenot@mines.edu>
2025-01-12 11:30:58 -0700

    docs: Comments in parser

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -4,17 +4,21 @@ pub struct Parser {
     tokenizer: Lexer
 }
 impl Parser {
+    // Create new parser
     pub fn new(stream: String) -> Parser {
         Parser {
             tokenizer: Lexer::new(stream)
         }
     }
+    // Mark position
     pub fn mark(&self) -> usize {
         self.tokenizer.mark()
     }
+    // Reset to position
     pub fn reset(&mut self, pos: usize) {
         self.tokenizer.reset(pos)
     }
+    // Expect a token
     pub fn expect(&mut self, arg: TokenType) -> Result<Option<&Token>, String> {
         let token = self.tokenizer.peek_token()?;
         if token.0 == arg {
