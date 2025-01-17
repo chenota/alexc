5b4c20b1e30280150b5d119458d1e7f2a740011d
chenota <achenot@mines.edu>
2025-01-16 22:26:31 -0700

    test: Added a bunch of tests to parser

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -19,4 +19,84 @@ mod parser_tests {
         // Return
         ()
     }
+    #[test]
+    fn int1() {
+        // Stream
+        let s = "100;".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
+        // Check values
+        match &x.1[0] {
+            Statement::ExprStatement(Expression::IntegerLiteral(v)) => assert_eq!(*v, 100),
+            _ => panic!()
+        }
+        // Return
+        ()
+    }
+    #[test]
+    fn int2() {
+        // Stream
+        let s = "(100);".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
+        // Check values
+        match &x.1[0] {
+            Statement::ExprStatement(Expression::IntegerLiteral(v)) => assert_eq!(*v, 100),
+            _ => panic!()
+        }
+        // Return
+        ()
+    }
+    #[test]
+    fn plus() {
+        // Stream
+        let s = "1+1;".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
+        // Check values
+        match &x.1[0] {
+            Statement::ExprStatement(Expression::BopExpression(Bop::PlusBop, _, _)) => (),
+            _ => panic!()
+        }
+        // Return
+        ()
+    }
+    #[test]
+    fn let_stmt() {
+        // Stream
+        let s = "let x = 0;".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
+        // Check values
+        match &x.1[0] {
+            Statement::LetStmt((s, None), _) => assert_eq!(s, "x"),
+            _ => panic!()
+        }
+        // Return
+        ()
+    }
+    #[test]
+    fn assign_stmt() {
+        // Stream
+        let s = "x = 0;".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
+        // Check values
+        match &x.1[0] {
+            Statement::AssignStmt(s, _) => assert_eq!(s, "x"),
+            _ => panic!()
+        }
+        // Return
+        ()
+    }
 }
