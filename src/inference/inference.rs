a31c574329655751aa1ce41ecb1de8b81f7f80f4
chenota <achenot@mines.edu>
2025-01-20 21:35:16 -0700

    feat: Added substitution to inference

--- src/inference/inference.rs
+++ src/inference/inference.rs
@@ -1,9 +1,46 @@
+use std::collections::HashMap;
+
+#[derive(Clone)]
 pub enum MonoType {
     Variable(usize),
     Function(TypeName, Vec<MonoType>)
 }
 
+#[derive(Clone)]
 pub enum TypeName {
     Int64,
     Char
+}
+
+pub type Context = HashMap<usize, MonoType>;
+pub type Substitution = HashMap<usize, MonoType>;
+
+pub trait Apply {
+    fn applym(&self, m: &MonoType) -> MonoType;
+    fn applyc(&self, c: &Context) -> Context;
+}
+
+impl Apply for Substitution {
+    fn applym(&self, m: &MonoType) -> MonoType {
+        match m {
+            MonoType::Variable(x) => match self.get(x) {
+                Some(x) => x.clone(),
+                None => m.clone()
+            },
+            MonoType::Function(name, ls) => MonoType::Function(
+                name.clone(), 
+                ls.iter().map(|r| self.applym(r)).collect()
+            )
+        }
+    }
+    fn applyc(&self, c: &Context) -> Context {
+        // New context
+        let mut new_c = Context::new();
+        // Apply self to each key-value pair
+        for (k, v) in c {
+            new_c.insert(*k, self.applym(v));
+        };
+        // Return
+        new_c
+    }
 }
