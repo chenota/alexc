b15c17e25a4ec0356aba08e4b2f8ea2505c7e12c
chenota <achenot@mines.edu>
2025-01-20 22:19:37 -0700

    test: Added tests for substitution combine function

--- src/inference/tests.rs
+++ src/inference/tests.rs
@@ -149,4 +149,44 @@ mod inference_tests {
         // Return
         ()
     }
+    #[test]
+    fn combine1() {
+        // Create substitution 1
+        let mut s1 = Substitution::new();
+        s1.insert(1, MonoType::Variable(2));
+        // Create substitution 2
+        let mut s2 = Substitution::new();
+        s2.insert(0, MonoType::Variable(1));
+        // Combine s1 and s2
+        let s3 = s1.combine(&s2);
+        // Check
+        assert_eq!(s3.len(), 2);
+        assert_eq!(*s3.get(&0).unwrap(), MonoType::Variable(2));
+        assert_eq!(*s3.get(&1).unwrap(), MonoType::Variable(2));
+        // Return
+        ()
+    }
+    #[test]
+    fn combine2() {
+        // Create substitution 1
+        let mut s1 = Substitution::new();
+        s1.insert(0, MonoType::Variable(1));
+        // Create substitution 2
+        let mut s2 = Substitution::new();
+        s2.insert(2, MonoType::Application(TypeName::Char, vec![MonoType::Variable(0)]));
+        // Combine s1 and s2
+        let s3 = s1.combine(&s2);
+        // Check
+        assert_eq!(s3.len(), 2);
+        assert_eq!(*s3.get(&0).unwrap(), MonoType::Variable(1));
+        match s3.get(&2).unwrap() {
+            MonoType::Application(TypeName::Char, v) => {
+                assert_eq!(v.len(), 1);
+                assert_eq!(v[0], MonoType::Variable(1));
+            },
+            _ => panic!()
+        };
+        // Return
+        ()
+    }
 }
