b5845d59eec706f5f8f3802023a9c421813ea0c5
chenota <achenot@mines.edu>
2025-02-04 19:55:32 -0700

    feat: Finished type synthesis for functions

--- src/typecheck/typecheck.rs
+++ src/typecheck/typecheck.rs
@@ -42,27 +42,27 @@ fn type_error(msg: String, loc: &(usize, usize)) -> String {
 }
 
 pub fn synth_type(c: &TypeContext, e: &Expression) -> Result<Type, String> {
-    match e.0 {
+    match &e.0 {
         ExpressionBody::IntLiteral(s, m) => {
             // Return OK Int by default
             Ok(Type::Int(
                 // If number is positive
                 if !s {
                     // Check smallest type that fits
-                    if m <= (i8::MAX as usize) { 3 }
-                    else if m <= (i16::MAX as usize) { 4 }
-                    else if m <= (i32::MAX as usize) { 5 }
-                    else if m <= (i64::MAX as usize) { 6 }
+                    if *m <= (i8::MAX as usize) { 3 }
+                    else if *m <= (i16::MAX as usize) { 4 }
+                    else if *m <= (i32::MAX as usize) { 5 }
+                    else if *m <= (i64::MAX as usize) { 6 }
                     else { return Err(type_error("Int literal too large".to_string(), &e.1)) }
                 } 
                 // Number is negative
                 else {
                     // Check largest negative type that fits
                     // Since 2's compliment, largest negative = largest positive + 1
-                    if m <= (i8::MAX as usize + 1) { 3 }
-                    else if m <= (i16::MAX as usize + 1) { 4 }
-                    else if m <= (i32::MAX as usize + 1) { 5 }
-                    else if m <= (i64::MAX as usize + 1) { 6 }
+                    if *m <= (i8::MAX as usize + 1) { 3 }
+                    else if *m <= (i16::MAX as usize + 1) { 4 }
+                    else if *m <= (i32::MAX as usize + 1) { 5 }
+                    else if *m <= (i64::MAX as usize + 1) { 6 }
                     else { return Err(type_error("Int literal too large".to_string(), &e.1)) }
                 }
             ))
@@ -78,14 +78,18 @@ pub fn synth_type(c: &TypeContext, e: &Expression) -> Result<Type, String> {
                 return Err(type_error("Invalid function call: ".to_string() + &f, &e.1)) 
             };
             // Check that arguments have correct type
-            ()
+            for (param, arg) in ftype.0.iter().zip(args) {
+                if check_type(param, c, arg).is_err() {
+                    return Err(type_error("Invalid argument: ".to_string() + &f, &e.1))
+                }
+            };
+            // Return output type of function
+            Ok(ftype.1.clone())
         }
         _ => panic!()
     }
 }
 
 pub fn check_type(t: &Type, c: &TypeContext, e: &Expression) -> Result<Type, String> {
-    match e {
-
-    }
+    Ok(Type::Int(0))
 }
