520ccd10f42c6f9c1d23e84341ef37e6ec422bad
Alex Chenot <alexchenot5@gmail.com>
2025-02-05 15:58:08 -0700

    feat: Added type synthesis for as expressions

--- src/typecheck/typecheck.rs
+++ src/typecheck/typecheck.rs
@@ -111,10 +111,8 @@ pub fn synth_type(c: &TypeContext, e: &Expression) -> Result<Type, String> {
             }
         },
         ExpressionBody::AsExpression(e1, t) => {
-            // Synthesize type for e1
-            let e1_type = synth_type(c, e1.as_ref())?;
-            // Check that e1 is a subtype of t
-            if e1_type > *t { return Err(type_error("Invalid typecast".to_string(), &e1.as_ref().1)) };
+            // Type check e1 against t
+            if !check_type(t, c, e1.as_ref())? { return Err(type_error("Invalid typecast".to_string(), &e1.as_ref().1)) }
             // Return t
             Ok(t.clone())
         },
