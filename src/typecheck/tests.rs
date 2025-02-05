a05f72b04dc0b9d4d6fb9c85c0f7ab201f1fbf5e
chenota <achenot@mines.edu>
2025-02-04 20:09:22 -0700

    test: Added tests for function call synthesis

--- src/typecheck/tests.rs
+++ src/typecheck/tests.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod typecheck_tests {
     use crate::typecheck::typecheck::*;
-    use crate::parser::parser::ExpressionBody;
+    use crate::parser::parser::{ExpressionBody, Parser};
 
     #[test]
     fn intord1() {
@@ -13,8 +13,14 @@ mod typecheck_tests {
     }
     #[test]
     fn intsynth1() {
+        // Stream
+        let s = "fun f -> i8 { return 0; }".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
         // Create new type context
-        let c = TypeContext::new();
+        let c = TypeContext::new(&x);
         // Create new expression
         let e = (ExpressionBody::IntLiteral(false, 127), (0, 0));
         // Sytnthesize type
@@ -22,11 +28,53 @@ mod typecheck_tests {
     }
     #[test]
     fn intsynth2() {
+        // Stream
+        let s = "fun f -> i8 { return 0; }".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
         // Create new type context
-        let c = TypeContext::new();
+        let c = TypeContext::new(&x);
         // Create new expression
         let e = (ExpressionBody::IntLiteral(false, 32766), (0, 0));
         // Sytnthesize type
         assert_eq!(synth_type(&c, &e).unwrap(), Type::Int(4))
+    }
+    #[test]
+    fn fcall1() {
+        // Stream
+        let s = "fun f -> i8 { return 0; }".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
+        // Create new type context
+        let c = TypeContext::new(&x);
+        // Create new expression
+        let e = (
+            ExpressionBody::CallExpression("f".to_string(), vec![]), 
+            (0, 0)
+        );
+        // Sytnthesize type
+        assert_eq!(synth_type(&c, &e).unwrap(), Type::Int(3))
+    }
+    #[test]
+    fn fcall2() {
+        // Stream
+        let s = "fun f(x: i8) -> i8 { return x; }".to_string();
+        // Token generator
+        let mut p = Parser::new(s);
+        // Parse
+        let x = p.parse().unwrap();
+        // Create new type context
+        let c = TypeContext::new(&x);
+        // Create new expression
+        let e = (
+            ExpressionBody::CallExpression("f".to_string(), vec![ (ExpressionBody::IntLiteral(false, 0), (0, 0)) ]), 
+            (0, 0)
+        );
+        // Sytnthesize type
+        assert_eq!(synth_type(&c, &e).unwrap(), Type::Int(3))
     }
 }
