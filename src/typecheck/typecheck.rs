2186e6fc7b7f017c148d94be7c0c15f57b5d5652
chenota <achenot@mines.edu>
2025-02-07 01:15:27 -0700

    feat: Added type check for ints

--- src/typecheck/typecheck.rs
+++ src/typecheck/typecheck.rs
@@ -127,5 +127,13 @@ pub fn synth_type(c: &TypeContext, e: &Expression) -> Result<Type, String> {
 }
 
 pub fn check_type(t: &Type, c: &TypeContext, e: &Expression) -> Result<bool, String> {
-    Ok(true)
+    match &e.0 {
+        ExpressionBody::IntLiteral(_, _) => {
+            // Synthesize type for integer
+            let t_synth = synth_type(c, e)?;
+            // Check that synthesized type is a subtype of t
+            Ok(t_synth <= *t)
+        },
+        _ => panic!()
+    }
 }
