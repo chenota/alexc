d7a0509b08ce666173ade3b68b511a3d37fcc2e1
Alex Chenot <alexchenot5@gmail.com>
2025-03-19 11:19:06 -0700

    feat: Added IR output functionality

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,33 @@ fn main() {
     // Parse arguments
     let args = Args::parse();
     // Read source file as string
-    let input = std::fs::read_to_string(args.source);
-    // 
+    let input = match std::fs::read_to_string(args.source) {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("{}", e.to_string());
+            std::process::exit(1)
+        }
+    };
+    // Check if in ir mode
+    if args.ir {
+        // Parse
+        let mut p = parser::parser::Parser::new(input);
+        let program = match p.parse() {
+            Ok(prog) => prog,
+            Err(s) => { eprintln!("{}", s); std::process::exit(1) }
+        };
+        // Generate IR from program
+        let ir = match program_to_ir(program) {
+            Ok(ir) => ir,
+            Err(s) => { eprintln!("{}", s); std::process::exit(1) }
+        };
+        // Write IR to file
+        match ir_to_file(ir.0, args.out) {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("{}", e.to_string());
+                std::process::exit(1)
+            }
+        };
+    }
 }
