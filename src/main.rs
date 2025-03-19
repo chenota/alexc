3557463868fcdf4b0f97f8f0f347db0f29036fcb
Alex Chenot <alexchenot5@gmail.com>
2025-03-19 10:55:56 -0700

    feat: Added command line parser for program

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ mod lexer;
 mod parser;
 mod codegen;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about="Compiler for the AlexC language", long_about = None)]
+struct Args {
+    // Source file
+    #[arg(required=true,help="Source file path")]
+    source: String,
+    // Output file name
+    #[arg(short='o',long="out",default_value="a.s",help="Output file path")]
+    out: String,
+    // Generate IR
+    #[arg(short='i',long="gen-ir",action,help="Generate intermediate representation code")]
+    ir: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Parse arguments
+    let args = Args::parse();
 }
